c83a6389683d0303fe1fa22f91455b8f11b716f1
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-08-11 23:41:48 +0530

    ok

--- restaurant-app/src/components/Layout/Footer.js
+++ restaurant-app/src/components/Layout/Footer.js
@@ -9,7 +9,18 @@ const Footer = () => {
     <>
       <Box sx={{textAlign: 'center', bgcolor: '#1A1A19', color:'white', p:3}}
       >
-        <Box>
+        <Box sx={{my:3, "& svg":{
+          fontSize:"60px",
+          cursor:"pointer",
+          mr:2,
+        },
+        "& svg:hover":{
+          color:'goldenrod',
+          transform:'translateX(5px)',
+          transition: 'all 400ms',
+        },
+        }}
+        >
            {/*icons*/} 
             <InstagramIcon />
             <TwitterIcon />
--- restaurant-app/src/components/Layout/Header.js
+++ restaurant-app/src/components/Layout/Header.js
@@ -96,7 +96,7 @@ const Header = () => {
       {drawer}
      </Drawer>
         </Box>
-        <Box sx={{p:1}}>
+        <Box>
         <Toolbar />
         </Box>
       </Box>
